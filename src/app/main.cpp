f2703b702de204d69cb981c65cb98e9319ba815b
Daniel Kiss <littlesnorrboy@gmail.com>
2018-02-05 21:00:01 +0100

    Init CMake

---
+++ src/app/main.cpp
@@ -0,0 +1,9 @@
+#include "hello.h"
+#include <iostream>
+
+int main()
+{
+	sayHi();
+	std::cin.ignore();
+	return 0;
+}
---
+++ src/core/hello/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+void sayHi()
+{
+	std::cout << "Hello\n";
+}
