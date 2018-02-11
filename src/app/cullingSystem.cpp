ac422b4d47418f26314911b238e3668a790b968d
unknown <dulyts@inf.elte.hu>
2018-02-11 18:12:25 +0100

    Mock culling

--- src/app/cullingSystem.cpp
+++ src/app/cullingSystem.cpp
@@ -1,10 +1,10 @@
 #include "cullingSystem.h"
 
 namespace mc {
-	core::Vector<Chunk> CullingSystem::getCulledChunks(float x, float y, float direction)
+	core::Vector<Chunk> CullingSystem::getCulledChunks(float x, float y, float direction, int distance)
 	{
-		auto culledChunks = core::Vector<Chunk>();
-
-		return culledChunks;
+		//auto culledChunks = core::Vector<Chunk>();
+		//return culledChunks;
+		return mCellSystem.getNearbyChunks((int)x, (int)y, distance);
 	}
 }
