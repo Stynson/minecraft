95ff8dbf6804fdff544dcfcee95e635f87288c00
Marton Kiss <mkiss004@gmail.com>
2018-02-11 20:57:46 +0100

    szevasztok

--- src/app/mapGenerator.cpp
+++ src/app/mapGenerator.cpp
@@ -1,16 +1,19 @@
 #include "mapGenerator.h"
 #include "core.h"
-#include <iostream>
+#include "iostream"
 
-namespace mc {
+namespace mc 
+{
 
 	Chunk MapGenerator::generate(int x, int y)
 	{
-		auto chunk = Chunk(x, y);
-		for (auto i = 0; i < Chunk::WIDTH; i++) {
-			for (auto j = 0; j < Chunk::WIDTH; j++) {
-				for (auto z = std::rand() % 255; z > 0; z--)
-				{
+		auto chunk = Chunk();
+		for (auto i = 0; i < Chunk::WIDTH; i++)
+		{
+			for (auto j = 0; j < Chunk::WIDTH; j++) 
+			{
+				for (auto z = mPerlinNoise.noise(i, j, 0); z > 0; z--)
+				{				
 					std::cout << "x:" << i << " y:" << j << "\nmagassag:" << z;
 					chunk.setBlock(BlockType::DIRT, i, z, j);
 				}
--- src/app/mapGenerator.h
+++ src/app/mapGenerator.h
@@ -1,14 +1,20 @@
 #pragma once
 
 #include "chunk.h"
+#include "perlin.h"
 
-namespace mc {
-	class MapGenerator {
+namespace mc
+{
+	class MapGenerator
+	{
 	public:
-		MapGenerator(int seed) : mSeed(seed) {};
+		MapGenerator(int seed)
+			: mSeed(seed)
+			, mPerlinNoise(seed)
+		{};
 		Chunk generate(int x, int y);
 	private:
 		int mSeed;
+		PerlinNoise mPerlinNoise;
 	};
-
 }
