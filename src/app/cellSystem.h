77badb852ca4869485075f61cbcf3e80e48e7973
Stynson <stynson@inf.elte.hu>
2018-02-18 09:34:15 +0100

    remove unused functions

--- src/app/cellSystem.h
+++ src/app/cellSystem.h
@@ -18,29 +18,11 @@ namespace mc {
 		Chunk* getChunk(int x, int z) ;
 		core::Vector<Chunk*> getNearbyChunks(const CameraData& cameraData);
 		core::Vector<Chunk*> getNearbyChunks(const CameraData& cameraData, int distance);
-		void setBlockType(BlockType type, glm::vec3 worldCoord);
-
-		void setBlock(glm::vec3 coords, BlockType toType)
-		{ 
-			auto chunk = getChunk(coords.x, coords.y, coords.z);
-			auto x = int(coords.x / mBlockSize) % Chunk::WIDTH;
-			auto y = int(coords.y / mBlockSize);
-			auto z = int(coords.z / mBlockSize) % Chunk::WIDTH;
-			chunk->setBlockType(toType, x, y, z);
-		}
+		void setBlockType(BlockType type, glm::vec3 worldCoord); 
 
 
 	private:
-		const int mBlockSize = 1;
-
-		Chunk* getChunk(float _x, float _y, float _z) const {
-			auto x = int(_x / mBlockSize / Chunk::WIDTH);
-			auto z = int(_z / mBlockSize / Chunk::WIDTH);
-			core::String key = std::to_string(x).c_str();
-			key += "_";
-			key += std::to_string(z).c_str();
-			return mMap.find(key)->second.get();
-		}
+		const int mBlockSize = 1; 
 		std::unordered_map<core::String, std::unique_ptr<Chunk>> mMap;
 		MapGenerator mGenerator;
 
